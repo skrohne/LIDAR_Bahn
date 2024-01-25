589f5b6d1add75a961d0e9aa1daa0091adfa2f79
Emil <emil.erlenkoetter@web.de>
2024-01-25 17:07:46 +0100

    Update viewer.R
    small changes

--- viewer.R
+++ viewer.R
@@ -1,12 +1,15 @@
 library(mapview)
-library(leafem)
 
 #Remove Z-Value
 ttops_norm<-st_zm(ttops_norm)
 
 # Define your custom color palette
-custom_palette <- c("darkgreen", "lightgreen", "yellow", "orange", "red")
+custom_palette <- c("darkgreen", "yellow", "orange", "red","darkred")
 
 # Create the map using mapview
-mapview(ttops_norm, zcol = "height_distance_diff", at = seq(0, 2.5, 0.5),
-        legend = TRUE, col.regions = custom_palette, alpha = 0)
+mapview(ttops_norm, zcol = "height_distance_diff",
+        map.types = c("CartoDB.Positron","Esri.WorldImagery"),
+        at = seq(0, 3.5,0.5),
+        legend = TRUE,
+        col.regions = custom_palette, 
+        alpha = 0,) #change Style
