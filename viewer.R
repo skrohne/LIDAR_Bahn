b39c621910a6fec17f2a8a8bdbd5385ab6dc1bf4
Severin Krohne <skrohne@uni-muenster.de>
2024-01-31 12:31:52 +0100

    average calculation and display

--- viewer.R
+++ viewer.R
@@ -7,10 +7,19 @@ ttops_norm<-st_zm(ttops_norm)
 custom_palette <- c("darkgreen", "yellow", "orange", "red","darkred")
 
 # Create the map using mapview
-mapview(ttops_norm, zcol = "height_distance_diff",
+mapview(ttops_try, zcol = "height_distance_diff",
         map.types = c("CartoDB.Positron","Esri.WorldImagery"),
         at = seq(0, 3.5,0.5),
         legend = TRUE,
         layer.name = 'Height-Distance-Index',
         col.regions = custom_palette, 
         alpha = 0) #change Style
+
+subset_map <- mapview(ttops_try, zcol = "average_height_diff",
+                map.types = c("CartoDB.Positron","Esri.WorldImagery"),
+                at = seq(0.3,0.6,0.1),
+                legend = TRUE,
+                layer.name = 'Subset Number',
+                col.regions = custom_palette, 
+                alpha = 0) #change Style
+subset_map
